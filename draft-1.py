922ec3e11adf698a0286c06a0cd8b9c524a19f7e
vicky <vixkyh@gmail.com>
2022-11-12 20:28:29 +0200

    halt - both functions for cross validation

--- draft-1.py
+++ draft-1.py
@@ -87,7 +87,7 @@ X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y, test_s
 # print(y_test.shape)
 
 
-#heatmap 
+# heatmap 
 
 # plt.figure(figsize=(10,10))
 # sns.heatmap(df.corr(),annot=True,fmt='.1f')
@@ -113,35 +113,75 @@ print("Accuracy score - ", metrics.accuracy_score(y_test,y_pred))
 # then it trains and evaluates the models 5 times, picking a different fold for evaluation every time and 
 # training on the other 4 folds.
 
+#k fold cross validation           , ---- email 
 
-cvscore = cross_val_score(nb, X, y, cv=5)
-print("Cross-validated scores:", cvscore)
-#print('Accuracy: %0.2f (+/- %0.2f)' % (cvscore.mean(), cvscore.std() * 2))
+from sklearn import model_selection
+from sklearn.model_selection import cross_validate
+from sklearn.metrics import recall_score , precision_score , accuracy_score , f1_score , roc_auc_score , confusion_matrix
+
+vc_results = cross_validate(nb, X_train, y_train, cv=5, scoring=('accuracy', 'precision', 'recall', 'f1', 'roc_auc'))
+print("!!Accuracy: %0.2f (+/- %0.2f)" % (vc_results['test_accuracy'].mean(), vc_results['test_accuracy'].std() * 2))
+
+#plot kfold progress
+
+from sklearn.model_selection import learning_curve
+train_sizes, train_scores, test_scores = learning_curve(nb, X_train, y_train, cv=5, scoring='accuracy', n_jobs=-1, train_sizes=np.linspace(0.01, 1.0, 50))
+train_mean = np.mean(train_scores, axis=1)
+train_std = np.std(train_scores, axis=1)
+test_mean = np.mean(test_scores, axis=1)
+test_std = np.std(test_scores, axis=1)
+plt.plot(train_sizes, train_mean, color='blue', marker='o', markersize=5, label='training accuracy')
+plt.fill_between(train_sizes, train_mean + train_std, train_mean - train_std, alpha=0.15, color='blue')
+plt.plot(train_sizes, test_mean, color='green', linestyle='--', marker='s', markersize=5, label='validation accuracy')
+plt.fill_between(train_sizes, test_mean + test_std, test_mean - test_std, alpha=0.15, color='green')
+plt.grid()
+plt.xlabel('Number of training samples')
+plt.ylabel('Accuracy')
+plt.legend(loc='lower right')
+plt.ylim([0.5, 1.0])
+plt.show()
+
+# kfold = model_selection.KFold(n_splits=5, random_state=42, shuffle=True)
+# results = model_selection.cross_val_score(nb, X_train, y_train, cv=kfold)
+# print(" !!!Accuracy: %.2f%% (%.2f%%)" % (results.mean()*100, results.std()*100))
+
+# # plt results from kfold
+
+# plt.figure(figsize=(10,10))
+# plt.plot(results)
+# plt.show()
+
+
+
+
+# cvscore = cross_val_score(nb, X, y, cv=5)
+# print("Cross-validated scores:", cvscore)
+# #print('Accuracy: %0.2f (+/- %0.2f)' % (cvscore.mean(), cvscore.std() * 2))
 
 #plot for the 5 folds
 
-plt.plot(cvscore)
-plt.title('5-fold cross validation')
-plt.xlabel('Fold')
-plt.ylabel('Accuracy')
-plt.show()
+# plt.plot(score)
+# plt.title('5-fold cross validation')
+# plt.xlabel('Fold')
+# plt.ylabel('Accuracy')
+# plt.show()
 
 #accuracy after cross validation
-print("Accuracy after cross validation: %0.2f (+/- %0.2f)" % (cvscore.mean(), cvscore.std() * 2))
+# print("Accuracy after cross validation: %0.2f (+/- %0.2f)" % (score.mean(), score.std() * 2))
 
 # geometric mean metric
 
-from sklearn.metrics import make_scorer
-from sklearn.model_selection import cross_validate
+# from sklearn.metrics import make_scorer
+# from sklearn.model_selection import cross_validate
 
-def geometric_mean(y_true, y_pred):
-    return metrics.fbeta_score(y_true, y_pred, beta=1, average='weighted')
+# def geometric_mean(y_true, y_pred):
+#     return metrics.fbeta_score(y_true, y_pred, beta=1, average='weighted')
 
 
-scoring = {'acc': 'accuracy', 
-              'prec_macro': 'precision_macro',
-                'rec_micro': 'recall_macro',
-                'f1_weighted': make_scorer(geometric_mean)}
+# scoring = {'acc': 'accuracy', 
+#               'prec_macro': 'precision_macro',
+#                 'rec_micro': 'recall_macro',
+#                 'f1_weighted': make_scorer(geometric_mean)}
 
 
 #print sensitivity and specificity
@@ -160,8 +200,6 @@ plot_confusion_matrix(nb, X_test, y_test)
 plt.show()
 
 
-
-
-print('The geometric mean is {}'.format(geometric_mean(
-    y_test,
-    y_pred)))
+# print('The geometric mean is {}'.format(geometric_mean(    # almost 51% accurate
+#     y_test,
+#     y_pred)))
